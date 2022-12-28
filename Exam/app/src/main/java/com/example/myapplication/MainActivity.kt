76cc5d26fa6dbd951039b7afb766ace1d3176d09
nokhyun90 <nokhyun90@gmail.com>
2022-12-28 22:33:46 +0900

    column, LazyColumn

--- Exam/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Exam/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,11 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
@@ -33,21 +36,50 @@ class MainActivity : ComponentActivity() {
 //                    }
 //                }
 //            }
-            Box(
+//            Box(
+//                modifier = Modifier
+//                    .background(color = Color.Green)
+//                    .fillMaxWidth()
+//                    .height(200.dp),
+//                contentAlignment = Alignment.TopEnd
+//            ) {
+//                Text(text = "Hello")
+//                Box(
+//                    modifier = Modifier
+//                        .fillMaxSize()
+//                        .padding(16.dp),
+//                    contentAlignment = Alignment.BottomEnd
+//                ) {
+//                    Text(text = "Hello", modifier = Modifier.padding(start = 20.dp))
+//                }
+//            }
+            val scrollState = rememberScrollState()
+
+            LazyColumn(
                 modifier = Modifier
-                    .background(color = Color.Green)
                     .fillMaxWidth()
-                    .height(200.dp),
-                contentAlignment = Alignment.TopEnd
+                    .background(color = Color.Green),
+                contentPadding = PaddingValues(16.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+//                    .verticalScroll(scrollState)
             ) {
-                Text(text = "Hello")
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(16.dp),
-                    contentAlignment = Alignment.BottomEnd
-                ) {
-                    Text(text = "Hello", modifier = Modifier.padding(start = 20.dp))
+//                for (i in 0 until 50) {
+//                    Text(text = i.toString(), modifier = Modifier.padding(12.dp))
+//                }
+                item {
+                    Text(text = "Header")
+                }
+
+                items(50) { index ->
+                    Text(
+                        text = index.toString(), modifier = Modifier
+                            .background(Color.Gray)
+                            .width(100.dp),
+                        textAlign = TextAlign.Center,
+                    )
+                }
+                item {
+                    Text(text = "Footer")
                 }
             }
         }
